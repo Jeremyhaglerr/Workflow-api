3c2d2d37a66d181cb1396d04225457d3a5bf671a
Jeremyhaglerr <jeremyhaglerr@gmail.com>
2022-04-11 14:49:03 -0500

    add update issue functionality

--- api/views/issues.py
+++ api/views/issues.py
@@ -27,3 +27,19 @@ def index():
 def show(id):
   issue = Issue.query.filter_by(id=id).first()
   return jsonify(issue.serialize()), 200
+
+@issues.route('/<id>', methods=["PUT"]) 
+@login_required
+def update(id):
+  data = request.get_json()
+  profile = read_token(request)
+  issue = Issue.query.filter_by(id=id).first()
+
+  if issue.profile_id != profile["id"]:
+    return 'Forbidden', 403
+
+  for key in data:
+    setattr(issue, key, data[key])
+
+  db.session.commit()
+  return jsonify(issue.serialize()), 200
